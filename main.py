08742c546a5b889fac23c8068275d03960d8bd7d
Secret-Plans <57553878+Secret-Plans@users.noreply.github.com>
2020-07-30 10:57:16 +1200

    added main file
    Importing ingredients and restrictions is implemented.

---
+++ main.py
@@ -0,0 +1,39 @@
+"""Main file
+By Damon Jones for 11PRG 1.7 2020
+"""
+# Imports
+import json
+import os
+
+
+# Functions
+def get_name() -> str:
+    user_name = input("")
+
+
+def load_ingredients(_dir : str) -> dict:
+    ingredients = {}
+    for filename in os.listdir(_dir):
+        with open(os.path.join(_dir, filename), "r") as f:
+            ingredients = {**ingredients, **json.load(f)}
+    return ingredients
+
+
+def load_restrictions(_dir : str) -> dict:
+    restrictions = {}
+    with open(os.path.join(_dir, "restrictions.json"), "r") as f:
+        restrictions = json.load(f)
+    return restrictions
+
+
+# Main
+def main() -> int:
+    ingredients = load_ingredients("Data/Ingredients")
+    restrictions = load_restrictions("Data")
+
+    
+    return 1
+
+
+if __name__ == "__main__":
+    main()
