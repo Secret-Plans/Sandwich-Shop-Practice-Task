dbb75a05cfe1fc54d39255d3c55b9332c97cad0f
Secret-Plans <57553878+Secret-Plans@users.noreply.github.com>
2020-07-31 11:48:16 +1200

    Basic class for an order implemented

---
+++ order.py
@@ -0,0 +1,14 @@
+"""Class representing a sandwich order.
+By Damon Jones for 11PRG 1.7 2020 Practice
+"""
+# Imports
+
+
+# Class
+class Order:
+    bread : str
+    meat : str
+    options : list
+
+    def __init__(self):
+        pass
