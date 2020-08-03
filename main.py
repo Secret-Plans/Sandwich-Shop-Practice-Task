60f35c39ebd440c963e38fd42561f9bded7a9f03
Secret-Plans <57553878+Secret-Plans@users.noreply.github.com>
2020-08-03 13:33:43 +1200

    added option selection and docstrings

--- main.py
+++ main.py
@@ -59,6 +59,10 @@ def get_meat(meats : list) -> str:
     Returns:
         str: The chosen meat.
     """
+    
+    # This function is effectively the same as get_bread except it works with meats instead.
+    # I didn't really feel like making one function that can do both with fancy logic so I
+    # just made two seperate functions for bread and meat.
     print("\nWhich Meat Would you Like?\n")
     for i in range(len(meats)):
         print(f"{i + 1}\t: {meats[i]}")
@@ -77,7 +81,75 @@ def get_meat(meats : list) -> str:
 
 
 def get_options(options : list) -> list:
-    pass
+    """Gets user input for what options they'd like.
+
+    Args:
+        options (list): List of options to choose from.
+
+    Returns:
+        list: The selected list of options for the order.
+    """
+    selections = [False] * len(options) #Initialize bool list - same size as the list of options
+
+    # Selections list is a list of booleans representing whether an option from options 
+    # is selected or not.
+
+    print("Which Options Would you Like?\n")
+    
+    selecting = True
+    while selecting:
+        for i in range(len(options)):
+            # I'm gonna break down this print statement so it's clear:
+            # I'm printing the name of the option along with it's index + 1 int the list.
+            # Before that I either print a space or a dash, depending on whether the option
+            # is selected or not. This works as I can make a list contain ' ' and '-' and
+            # index it using a boolean from the selections list, as False counts as 0 and
+            # True counts as 1.
+            print(f" {[' ', '-'][selections[i]]} {i + 1}\t: {options[i]}")
+        
+        print("\nEnter a number to toggle option")
+        print("Enter nothing to continue\n")
+
+        user_in = input(f"Enter a Number Between 1 and {len(options)}: ")
+        print("\n\n\n\n\n")
+
+        # Check whether input is a valid integer and if so do stuff with it.
+        try:
+            user_in = int(user_in) #Turn user input into integer
+            if selections[user_in - 1]:
+                selections[user_in - 1] = False
+            elif check_number_of_selections(selections) < 4: #Make sure user doesn't have too many options.
+                selections[user_in - 1] = True
+        except:
+            # Ends loop if user input is empty.
+            if user_in == "":
+                selecting = False
+    
+    selected_options = [] #Final list of options that have been selected
+    # This loop just add an option to the final list of options if it's been selected.
+    for i in range(len(options)):
+        if selections[i]:
+            selected_options.append(options[i])
+    return selected_options
+
+
+def check_number_of_selections(selections : list) -> int:
+    """Checks number of options selected in a list.
+
+    This is used to ensure that the user doesn't select more than four options, as
+    the task suggests the program should.
+
+    Args:
+        selections (list): List of bools representing which options are selected.
+
+    Returns:
+        int: Number of options selected.
+    """
+    number_of_selections = 0
+    for item in selections:
+        if item:
+            number_of_selections += 1
+    return number_of_selections
 
 
 # Main
@@ -89,9 +161,12 @@ def main() -> int:
     sandwich.bread = get_bread(ingredients["breads"])
     print("\n\n\n")
     sandwich.meat = get_meat(ingredients["meats"])
+    print("\n\n\n")
+    sandwich.options = get_options(ingredients["options"])
 
     print(sandwich.bread)
     print(sandwich.meat)
+    print(sandwich.options)
 
     return 1
 
